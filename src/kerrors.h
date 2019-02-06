3f7384cfc7e9de8d2fa05b865cd887947b11037d
Sapein <sapeint@hashbang.sh>
2019-02-05 21:40:40 -0500

    Add in a Panic Facility similar to KLog

--- src/kerrors.h
+++ src/kerrors.h
@@ -2,7 +2,7 @@
 #define SVSH_KERRORS
 #include <stdio.h>
 #define Panic() SVSH_Kernel_Panic("%s() has caused an error. It called in %s at %d\n", __func__, __FILE__, __LINE__)
-#define KLog(x, msg) fprintf(stderr, "%s: %s", (x), (msg))
+#define KPanic(x, msg) SVSH_Kernel_Panic("%s: %s", (x), (msg))
 #define KERROR
 
 _Noreturn void SVSH_Kernel_Panic(char *fmt, ...);
