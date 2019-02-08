573e4fe2257d554a044dc8d0671e981c7f3ec39d
Sapein <sapeint@hashbang.sh>
2019-02-07 21:29:44 -0500

    Add in varadic macros

--- src/kerrors.h
+++ src/kerrors.h
@@ -2,8 +2,10 @@
 #define SVSH_KERRORS
 #include <stdio.h>
 #define Panic() SVSH_Kernel_Panic("%s() has caused an error. It called in %s at %d\n", __func__, __FILE__, __LINE__)
+#define KVPanic(x, fmt, ...) SVSH_Kernel_Panic("%s: " fmt, (x), __VA_ARGS__)
 #define KPanic(x, msg) SVSH_Kernel_Panic("%s: %s", (x), (msg))
-#define KERROR
+#define KVLog(x, fmt, ...) fprintf(stderr, "%s: " fmt, (x), __VA_ARGS__)
+#define KLog(x, msg) fprintf(stderr, "%s: %s", (x), (msg))
 
 _Noreturn void SVSH_Kernel_Panic(char *fmt, ...);
 
